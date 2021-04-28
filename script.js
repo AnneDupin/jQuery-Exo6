34708d27114b981559f80babcda8826ccf238333
Anne <anne.dupin91@gmail.com>
2021-04-28 15:04:55 +0200

    First

---
+++ script.js
@@ -0,0 +1,12 @@
+// $('.a_cacher').click(function(){
+// $('.a_cacher').hide();
+// console.log('Élément .a_cacher est caché');
+// });
+
+// Autre méthode
+
+$(".a_cacher").hover(function(event){
+    $(this).hide();
+     /*OR*/
+    $(".a_cacher").hide();
+   });
